4012352aa9d4efcf6af959978a18c29f179ede0d
Antoni Nicyfor <ant.nicyf@gmail.com>
2022-11-10 16:30:29 -0800

    Implement playfield drawing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,40 @@
 use raylib::prelude::*;
 
+struct GameState
+{
+    landed: [[bool; 10]; 16]
+}
+
+impl GameState
+{
+    fn new() -> GameState
+    {
+        GameState { landed: [[false; 10]; 16] }
+    }
+}
+
 fn main() {
     let (mut rl, thread) = raylib::init()
         .size(640, 480)
-        .title("Hello, World")
+        .title("Rusty Blocks")
         .build();
+
+    let mut game_state = GameState::new();
+    game_state.landed[0][0] = true;
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
 
         d.clear_background(Color::WHITE);
-        d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
+        for (x, row) in game_state.landed.iter().enumerate()
+        {
+            for (y, col) in row.iter().enumerate()
+            {
+                if *col
+                {
+                    d.draw_rectangle((x * 16) as i32, (y * 16) as i32, 16, 16, Color::BLUE);
+                }
+            }
+        }
     }
 }
