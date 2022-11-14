217011390c1c95e83d75f07a7983f375a1fc6b30
Antoni Nicyfor <ant.nicyf@gmail.com>
2022-11-14 11:33:28 -0800

    Move rendering into seperate function, add resolution and scaling constants

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use raylib::prelude::*;
 
+const PIXEL_RESOLUTION_X: i32 = 10;
+const PIXEL_RESOLUTION_Y: i32 = 16;
+const RES_MULTIPLIER: i32 = 16;
+
 struct GameState {
     landed: [[bool; 16]; 10],
     block: Block,
@@ -33,36 +37,47 @@ impl Block {
 }
 
 fn main() {
-    let (mut rl, thread) = raylib::init().size(640, 480).title("Rusty Blocks").build();
+    let (mut rl, thread) = raylib::init()
+        .size(
+            PIXEL_RESOLUTION_X * RES_MULTIPLIER,
+            PIXEL_RESOLUTION_Y * RES_MULTIPLIER,
+        )
+        .title("Rusty Blocks")
+        .build();
 
     let mut game_state = GameState::new();
     game_state.landed[0][15] = true;
 
     while !rl.window_should_close() {
-        let mut d = rl.begin_drawing(&thread);
+        render(&mut rl, &thread, &game_state);
+        
+    }
+}
 
-        d.clear_background(Color::WHITE);
-        //Draw landed tiles
-        for (x, row) in game_state.landed.iter().enumerate() {
-            for (y, col) in row.iter().enumerate() {
-                if *col {
-                    d.draw_rectangle((x * 16) as i32, (y * 16) as i32, 16, 16, Color::BLUE);
-                }
+fn render(rl: &mut RaylibHandle, thread: &RaylibThread, game_state: &GameState) {
+    let mut d = rl.begin_drawing(&thread);
+
+    d.clear_background(Color::WHITE);
+    //Draw landed tiles
+    for (x, row) in game_state.landed.iter().enumerate() {
+        for (y, col) in row.iter().enumerate() {
+            if *col {
+                d.draw_rectangle((x * 16) as i32, (y * 16) as i32, 16, 16, Color::BLUE);
             }
         }
+    }
 
-        //Draw current block
-        for (x, row) in game_state.block.shape.iter().enumerate() {
-            for (y, col) in row.iter().enumerate() {
-                if *col == 1 {
-                    d.draw_rectangle(
-                        ((x as i32 + game_state.block.pos_x) * 16),
-                        ((y as i32 + game_state.block.pos_y) * 16),
-                        16,
-                        16,
-                        Color::BLACK,
-                    );
-                }
+    //Draw current block
+    for (x, row) in game_state.block.shape.iter().enumerate() {
+        for (y, col) in row.iter().enumerate() {
+            if *col == 1 {
+                d.draw_rectangle(
+                    (x as i32 + game_state.block.pos_x) * RES_MULTIPLIER,
+                    (y as i32 + game_state.block.pos_y) * RES_MULTIPLIER,
+                    RES_MULTIPLIER,
+                    RES_MULTIPLIER,
+                    Color::BLACK,
+                );
             }
         }
     }
