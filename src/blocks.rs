7fe57e6ff8de6cf94fded990af00b223c07a3878
Antoni Nicyfor <ant.nicyf@gmail.com>
2022-11-14 21:34:02 -0800

    Add basic block rotation without collision detection, along with all block types

--- src/blocks.rs
+++ src/blocks.rs
@@ -1,17 +1,139 @@
+
+pub enum BlockTypes {
+    I,
+    O,
+    T,
+    S,
+    Z,
+    L,
+    J,
+}
+
+pub static I_ROTATIONS: [[[i32; 4]; 4]; 2] = [
+    [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]],
+    [[0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0]],
+];
+
+pub static S_ROTATIONS: [[[i32; 4]; 4]; 2] = [
+    [[0, 0, 0, 0], [0, 0, 0, 0], [0, 1, 1, 0], [1, 1, 0, 0]],
+    [[0, 0, 0, 0], [1, 0, 0, 0], [1, 1, 0, 0], [0, 1, 0, 0]],
+];
+
+pub static Z_ROTATIONS: [[[i32; 4]; 4]; 2] = [
+    [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 0, 0], [0, 1, 1, 0]],
+    [[0, 0, 0, 0], [0, 1, 0, 0], [1, 1, 0, 0], [1, 0, 0, 0]],
+];
+
+pub static O_ROTATIONS: [[[i32; 4]; 4]; 1] = [[[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]]];
+
+pub static T_ROTATIONS: [[[i32; 4]; 4]; 4] = [
+    [[0, 0, 0, 0], [0, 1, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0]],
+    [[0, 0, 0, 0], [0, 1, 0, 0], [1, 1, 0, 0], [0, 1, 0, 0]],
+    [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 0], [0, 1, 0, 0]],
+    [[0, 0, 0, 0], [0, 1, 0, 0], [0, 1, 1, 0], [0, 1, 0, 0]],
+];
+
+pub static J_ROTATIONS: [[[i32; 4]; 4]; 4] = [
+    [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 0], [0, 0, 1, 0]],
+    [[0, 0, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [1, 1, 0, 0]],
+    [[0, 0, 0, 0], [1, 0, 0, 0], [1, 1, 1, 0], [0, 0, 0, 0]],
+    [[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 0, 0], [0, 1, 0, 0]],
+];
+
+pub static L_ROTATIONS: [[[i32; 4]; 4]; 4] = [
+    [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 0], [1, 0, 0, 0]],
+    [[0, 0, 0, 0], [1, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]],
+    [[0, 0, 0, 0], [0, 0, 1, 0], [1, 1, 1, 0], [0, 0, 0, 0]],
+    [[0, 0, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 1, 0]],
+];
+
 pub struct Block {
     pub pos_x: i32,
     pub pos_y: i32,
     pub shape: [[i32; 4]; 4],
+    pub current_rotation: i32,
+    pub block_type: BlockTypes,
 }
 
 impl Block {
-    pub fn new_square() -> Block {
+    pub fn new(block_type: BlockTypes) -> Block {
+        let shape: [[i32; 4]; 4];
+
+        match block_type {
+            BlockTypes::I => shape = I_ROTATIONS[0],
+            BlockTypes::O => shape = O_ROTATIONS[0],
+            BlockTypes::T => shape = T_ROTATIONS[0],
+            BlockTypes::S => shape = S_ROTATIONS[0],
+            BlockTypes::Z => shape = Z_ROTATIONS[0],
+            BlockTypes::J => shape = J_ROTATIONS[0],
+            BlockTypes::L => shape = L_ROTATIONS[0],
+        }
+
         let block = Block {
             pos_x: 0,
             pos_y: 0,
-            shape: [[1, 1, 0, 0], [1, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]],
+            shape,
+            current_rotation: 0,
+            block_type
         };
 
         block
     }
+
+    pub fn rotate(&mut self, clock_wise: bool) {
+        let value: i32;
+        if clock_wise { value = 1 } else { value = -1 }
+
+        self.current_rotation = wrapping_add(self.current_rotation, value, &self.block_type);
+        match self.block_type {
+            BlockTypes::I => self.shape = I_ROTATIONS[self.current_rotation as usize],
+            BlockTypes::O => self.shape = O_ROTATIONS[self.current_rotation as usize],
+            BlockTypes::T => self.shape = T_ROTATIONS[self.current_rotation as usize],
+            BlockTypes::S => self.shape = S_ROTATIONS[self.current_rotation as usize],
+            BlockTypes::Z => self.shape = Z_ROTATIONS[self.current_rotation as usize],
+            BlockTypes::J => self.shape = J_ROTATIONS[self.current_rotation as usize],
+            BlockTypes::L => self.shape = L_ROTATIONS[self.current_rotation as usize],
+        }
+    }
+}
+
+
+pub fn wrapping_add(x: i32, y: i32, block_type: &BlockTypes) -> i32 {
+    let mut result: i32;
+
+    match block_type {
+            BlockTypes::I => {
+                result = x + y;
+                if result > 1 { result = 0 }
+                if result < 0 { result = 1 }
+            },
+            BlockTypes::O => result = 0,
+            BlockTypes::T => {
+                result = x + y;
+                if result > 3 { result = 0 }
+                if result < 0 { result = 3 }
+            },
+            BlockTypes::S => {
+                result = x + y;
+                if result > 3 { result = 0 }
+                if result < 0 { result = 3 }
+            },
+            BlockTypes::Z => {
+                result = x + y;
+                if result > 3 { result = 0 }
+                if result < 0 { result = 3 }
+            },
+            BlockTypes::J => {
+                result = x + y;
+                if result > 3 { result = 0 }
+                if result < 0 { result = 3 }
+            },
+            BlockTypes::L => {
+                result = x + y;
+                if result > 3 { result = 0 }
+                if result < 0 { result = 3 }
+            },
+        }
+
+    result
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use raylib::prelude::*;
 mod blocks;
 use blocks::*;
 
-const PIXEL_RESOLUTION_X: i32 = 10;
-const PIXEL_RESOLUTION_Y: i32 = 16;
-const RES_MULTIPLIER: i32 = 16;
+//Size of screen in tiles
+const TILE_RESOLUTION_X: i32 = 10;
+const TILE_RESOLUTION_Y: i32 = 16;
+//Resolution of tiles
+const RES_MULTIPLIER: i32 = 32;
 
-struct GameState {
+pub struct GameState {
     landed: [[bool; 16]; 10],
     block: Block,
 }
@@ -16,7 +18,7 @@ impl GameState {
     fn new() -> GameState {
         GameState {
             landed: [[false; 16]; 10],
-            block: Block::new_square(),
+            block: Block::new(BlockTypes::I),
         }
     }
 }
@@ -24,14 +26,13 @@ impl GameState {
 fn main() {
     let (mut rl, thread) = raylib::init()
         .size(
-            PIXEL_RESOLUTION_X * RES_MULTIPLIER,
-            PIXEL_RESOLUTION_Y * RES_MULTIPLIER,
+            TILE_RESOLUTION_X * RES_MULTIPLIER,
+            TILE_RESOLUTION_Y * RES_MULTIPLIER,
         )
         .title("Rusty Blocks")
         .build();
 
     let mut game_state = GameState::new();
-    game_state.landed[0][15] = true;
 
     while !rl.window_should_close() {
         render(&mut rl, &thread, &game_state);
@@ -39,6 +40,7 @@ fn main() {
     }
 }
 
+/// Main render function
 fn render(rl: &mut RaylibHandle, thread: &RaylibThread, game_state: &GameState) {
     let mut d = rl.begin_drawing(&thread);
 
@@ -47,7 +49,13 @@ fn render(rl: &mut RaylibHandle, thread: &RaylibThread, game_state: &GameState) 
     for (x, row) in game_state.landed.iter().enumerate() {
         for (y, col) in row.iter().enumerate() {
             if *col {
-                d.draw_rectangle((x * 16) as i32, (y * 16) as i32, 16, 16, Color::BLUE);
+                d.draw_rectangle(
+                    x as i32 * RES_MULTIPLIER,
+                    y as i32 * RES_MULTIPLIER,
+                    RES_MULTIPLIER,
+                    RES_MULTIPLIER,
+                    Color::BLUE,
+                );
             }
         }
     }
@@ -68,34 +76,42 @@ fn render(rl: &mut RaylibHandle, thread: &RaylibThread, game_state: &GameState) 
     }
 }
 
+/// Main game tick functions
 fn tick(game_state: &mut GameState, rl: &mut RaylibHandle) {
+    //Handle input
     match rl.get_key_pressed() {
         None => (),
         Some(x) => match x {
             KeyboardKey::KEY_D => try_move(game_state, 1, 0),
             KeyboardKey::KEY_A => try_move(game_state, -1, 0),
             KeyboardKey::KEY_S => try_move(game_state, 0, 1),
+            KeyboardKey::KEY_J => try_rotate(game_state, false),
+            KeyboardKey::KEY_K => try_rotate(game_state, true),
             _ => (),
         },
     }
 }
 
+/// Attempt to move block, landing if hitting the bottom, or moving block if not
 fn try_move(game_state: &mut GameState, dx: i32, dy: i32) {
     let new_x = game_state.block.pos_x + dx;
     let new_y = game_state.block.pos_y + dy;
-    println!("{}", new_y);
 
     for (x, row) in game_state.block.shape.iter().enumerate() {
         for (y, col) in row.iter().enumerate() {
             if *col == 1 {
-                if new_x as usize + x > 9 {
+                //Prevent Blocks from being moved off the edge of the screen
+                //Also restricts new position values to -1 as lowest, preventing overflow errors
+                if new_x + (x as i32) > 9 || new_x + (x as i32) < 0 {
                     return;
                 }
-                if new_y as usize + y > 15 {
+                if new_y + y as i32 > 15 {
                     land_block(game_state);
                     return;
                 }
-                if game_state.landed[x + new_x as usize][y + new_y as usize] {
+                //Positions can be negative, so juggle type casts to prevent overflows
+                //This is ok because new_x will never be less than -1
+                if game_state.landed[(x as i32 + new_x) as usize][(y as i32 + new_y) as usize] {
                     land_block(game_state);
                     return;
                 }
@@ -107,15 +123,34 @@ fn try_move(game_state: &mut GameState, dx: i32, dy: i32) {
     game_state.block.pos_x += dx;
 }
 
+fn try_rotate(game_state: &mut GameState, clock_wise: bool) {
+        let value: i32;
+        if clock_wise { value = 1 } else { value = -1 }
+
+        game_state.block.current_rotation = wrapping_add(game_state.block.current_rotation, value, &game_state.block.block_type);
+        match game_state.block.block_type {
+            BlockTypes::I => game_state.block.shape = I_ROTATIONS[game_state.block.current_rotation as usize],
+            BlockTypes::O => game_state.block.shape = O_ROTATIONS[game_state.block.current_rotation as usize],
+            BlockTypes::T => game_state.block.shape = T_ROTATIONS[game_state.block.current_rotation as usize],
+            BlockTypes::S => game_state.block.shape = S_ROTATIONS[game_state.block.current_rotation as usize],
+            BlockTypes::Z => game_state.block.shape = Z_ROTATIONS[game_state.block.current_rotation as usize],
+            BlockTypes::J => game_state.block.shape = J_ROTATIONS[game_state.block.current_rotation as usize],
+            BlockTypes::L => game_state.block.shape = L_ROTATIONS[game_state.block.current_rotation as usize],
+        }
+    }
+
+
+/// Adds block to landed playfield, generates new block
 fn land_block(game_state: &mut GameState) {
     for (x, row) in game_state.block.shape.iter().enumerate() {
         for (y, col) in row.iter().enumerate() {
             if *col == 1 {
-                game_state.landed[game_state.block.pos_x as usize + x]
+                //Juggle number types to prevent overflow
+                game_state.landed[(game_state.block.pos_x + x as i32) as usize]
                     [game_state.block.pos_y as usize + y] = true;
             }
         }
     }
 
-    game_state.block = Block::new_square();
+    game_state.block = Block::new(BlockTypes::O);
 }
